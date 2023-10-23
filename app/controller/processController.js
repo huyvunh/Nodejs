15d46da473d62aab4babb86e873b10d06b8e1a71
huyvunh <huyvu.nh02@gmail.com>
2023-10-23 07:57:25 +0700

    origin

--- app/controller/processController.js
+++ app/controller/processController.js
@@ -10,24 +10,51 @@ const binance = new Binance().options({
     APISECRET: process.env.BINANCE_API_SECRET,
 });
 
-const { sma_indicator_ct, rsi_indicator_ct } = require('../indicator/indicator')
+const { sma_indicator_ct,
+    rsi_indicator_ct,
+    rsi_indicator_ct_lib,
+    ema_indicator_ct,
+    macd_indicator_ct,
+    macd_indicator_ct_lib
+} = require('../indicator/indicator')
 
 const sma_inc = (data, period) => {
     const result = sma_indicator_ct(data, period);
     return data = data.map((d, i) => ({ ...d, sma: result[i] == null ? '' : result[i] }));
 }
 
-const rsi_inc = (data, period) => {
+const rsi_inc = async (data, period) => {
     const result = rsi_indicator_ct(data, period);
     return data = data.map((d, i) => ({ ...d, rsi: result[i] == null ? '' : result[i] }));
 }
+const rsi_inc_async = async (data, period) => {
+    const result = await rsi_indicator_ct_lib(data, period);
+    return data = data.map((d, i) => ({ ...d, rsi_lib: result[i] == null ? '' : result[i] }));
+}
 
+
+
+const ema_inc = (data, period) => {
+    const result = ema_indicator_ct(data, period);
+    return data = data.map((d, i) => ({ ...d, ema: result[i] == null ? '' : result[i] }));
+}
+
+const macd_inc = async (data, shortPeriod, longPeriod, signalPeriod) => {
+    const result = await macd_indicator_ct_lib(data, shortPeriod, longPeriod, signalPeriod);
+    //console.log(result.macd_histogram)
+    return data = data.map((d, i) => ({
+        ...d,
+        macd_histogram: result.macd_histogram[i],
+        macd_fast: result.macd_fast[i],
+        macd_slow: result.macd_slow[i],
+    }));
+}
 
 exports.LongShortBinance = async () => {
     console.log("1231423432432432")
     const chatId = -4075927560;
 
-    binance.futuresChart('ETHUSDT', '1m', async (symbol, interval, chart) => {
+    binance.futuresChart('ETHUSDT', '30m', async (symbol, interval, chart) => {
         const data = Object.entries(chart);
         //console.log(data)
         let chartData = data.map((d) => ({
@@ -41,9 +68,12 @@ exports.LongShortBinance = async () => {
             low: parseFloat(d[1].low),
             volume: parseFloat(d[1].volume),
         }))
-        // console.log(chartData)
-        chartData = sma_inc(chartData, 100)
-        chartData = rsi_inc(chartData, 14)
+        //console.log(chartData)
+        // chartData = sma_inc(chartData, 100)
+        chartData = await rsi_inc(chartData, 24)
+        chartData = await rsi_inc_async(chartData, 24)
+        //chartData = ema_inc(chartData, 10)
+        //chartData = await macd_inc(chartData, 12, 26, 9)
         console.table(chartData)
 
     })
--- app/indicator/indicator.js
+++ app/indicator/indicator.js
@@ -1,6 +1,32 @@
 
 const Decimal = require('decimal.js');
+const tulind = require('tulind');
+const { promisify } = require('util');
 
+const sma_async = promisify(tulind.indicators.sma.indicator);
+const ema_async = promisify(tulind.indicators.ema.indicator);
+const macd_async = promisify(tulind.indicators.macd.indicator);
+const rsi_async = promisify(tulind.indicators.rsi.indicator);
+
+const ema_indicator_ct_lib = async (data, period) => {
+    const closePrices = data.map((d) => d.close);
+    const results = await ema_async([closePrices], [period]);
+    const d2 = results[0];
+    const diff = data.length - d2.length;
+    const emptyArray = [...new Array(diff)].map((d) => '');
+    const d3 = [...emptyArray, ...d2];
+    return d3;
+};
+
+const sma_indicator_ct_lib = async (data, period) => {
+    const closePrices = data.map((d) => d.close);
+    const results = await sma_async([closePrices], [period]);
+    const d2 = results[0];
+    const diff = data.length - d2.length;
+    const emptyArray = [...new Array(diff)].map((d) => '');
+    const d3 = [...emptyArray, ...d2];
+    return d3;
+};
 
 const sma_indicator_ct = (data, period) => {
     const SMA_period = period;
@@ -84,7 +110,162 @@ const rsi_indicator_ct = (data, period) => {
 
 }
 
+const ema_indicator_ct = (data, period) => {
+    const EMA_period = period;
+    const smoothing = 2 / (EMA_period + 1)
+    const avgPrices = data.map((day) =>
+        Number(parseFloat(new Decimal(day.open).plus(day.close).plus(day.low).plus(day.high).dividedBy(4)).toFixed(8))
+    );
+
+    // Tính EMA
+    const EMA = []
+    let prevEMA = null
+
+    for (let i = 0; i < avgPrices.length; i++) {
+
+        if (i < EMA_period) {
+            EMA.push(null)
+        } else {
+            const currEMA = (avgPrices[i] - prevEMA) * smoothing + prevEMA
+            EMA.push(currEMA)
+            prevEMA = currEMA
+        }
+    }
+
+    return EMA;
+}
+const macd_indicator_ct = (data, shortPeriod, longPeriod, signalPeriod) => {
+
+    // const EMA_short = [];
+    // const EMA_long = [];
+    // const MACD_Line = [];
+
+    // let emaShort = shortPeriod;
+    // let emaLong = longPeriod;
+    // if (data.length < longPeriod) {
+    //     return null; // Không đủ dữ liệu để tính MACD Line
+    // }
+    // const multiplier = 2 / (shortPeriod + 1);
+
+    // for (let i = 0; i < data.length; i++) {
+    //     const closePrice = Number(parseFloat(new Decimal(data[i].close).toFixed(8)));
+
+    //     // Tính đường EMA ngắn hạn (EMA12)
+    //     if (i < shortPeriod) {
+    //         emaShort += closePrice;
+    //         EMA_short.push(null); // Chưa đủ dữ liệu để tính EMA
+    //     } else {
+    //         if (i === shortPeriod) {
+    //             emaShort /= shortPeriod;
+    //         } else {
+    //             emaShort = (closePrice - emaShort) * multiplier + emaShort;
+    //         }
+    //         EMA_short.push(emaShort);
+    //     }
+
+    //     // Tính đường EMA dài hạn (EMA26)
+    //     if (i < longPeriod) {
+    //         emaLong += closePrice;
+    //         EMA_long.push(null); // Chưa đủ dữ liệu để tính EMA
+    //     } else {
+    //         if (i === longPeriod) {
+    //             emaLong /= longPeriod;
+    //         } else {
+    //             emaLong = (closePrice - emaLong) * multiplier + emaLong;
+    //         }
+    //         EMA_long.push(emaLong);
+    //     }
+
+    //     // Tính đường MACD Line
+    //     if (EMA_short[i] !== null && EMA_long[i] !== null) {
+    //         MACD_Line.push(EMA_short[i] - EMA_long[i]);
+    //     } else {
+    //         MACD_Line.push(null); // Chưa đủ dữ liệu để tính MACD Line
+    //     }
+    // }
+
+    // // console.log(MACD_Line);
+    // return MACD_Line;
+
+    let closePrices = data.map((day) => day.close);
+
+
+    // Tính EMA ngắn hạn
+    const shortEma = []
+    let prevShortEma = null
+    for (let i = 0; i < closePrices.length; i++) {
+        if (i < shortPeriod) {
+            shortEma.push(null)
+        } else {
+            const currentEma = (closePrices[i] - prevShortEma) * (2 / (shortPeriod + 1)) + prevShortEma
+            shortEma.push(currentEma)
+            prevShortEma = currentEma
+        }
+    }
+    // Tính EMA dài hạn 
+    const longEma = []
+    let prevLongEma = null
+    for (let i = 0; i < closePrices.length; i++) {
+        if (i < longPeriod) {
+            longEma.push(null)
+        } else {
+            const currentEma = (closePrices[i] - prevLongEma) * (2 / (longPeriod + 1)) + prevLongEma
+            longEma.push(currentEma)
+            prevLongEma = currentEma
+        }
+    }
+
+    // Tính đường MACD 
+    const macdLine = []
+    for (let i = 0; i < longEma.length; i++) {
+        macdLine.push(shortEma[i] - longEma[i])
+    }
+
+    // Tính đường Signal 
+    const signalLine = []
+    let prevSignal = null
+    for (let i = 0; i < macdLine.length; i++) {
+        if (i < signalPeriod) {
+            signalLine.push(null)
+        } else {
+            const currentSignal = (macdLine[i] - prevSignal) * (2 / (signalPeriod + 1)) + prevSignal
+            signalLine.push(currentSignal)
+            prevSignal = currentSignal
+        }
+    }
+
+    return { signalLine, shortEma, longEma };
+}
+
+const rsi_indicator_ct_lib = async (data, period) => {
+    const closePrices = data.map((d) => d.close);
+    const results = await rsi_async([closePrices], [period]);
+    const d2 = results[0];
+    const diff = data.length - d2.length;
+    const emptyArray = [...new Array(diff)].map((d) => '');
+    const d3 = [...emptyArray, ...d2];
+    return d3;
+}
+
+const macd_indicator_ct_lib = async (data, shortPeriod, longPeriod, signalPeriod) => {
+
+    const closePrices = data.map((day) => day.close);
+    const result = await macd_async([closePrices], [shortPeriod, longPeriod, signalPeriod]);
+    const diff = data.length - result[0].length;
+    const empty = [...new Array(diff)].map((d) => '');
+
+    const macd_fast = [...empty, ...result[0]];
+    const macd_slow = [...empty, ...result[1]];
+    const macd_histogram = [...empty, ...result[2]];
+    //console.log('sadasdas',macd_histogram)
+    return { macd_fast, macd_slow, macd_histogram };
+}
+
 module.exports = {
     sma_indicator_ct,
-    rsi_indicator_ct
+    rsi_indicator_ct,
+    rsi_indicator_ct_lib,
+    ema_indicator_ct,
+    macd_indicator_ct,
+    macd_indicator_ct_lib
 }
